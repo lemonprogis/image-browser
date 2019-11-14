1d1a22a67ad46a3c2478bac025a1dfb9238f900e
Ed <edward.briggler@gmail.com>
2019-11-14 09:21:14 -0600

    added steps to create image browser with electron

---
+++ src/app/images.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+const electron = (<any>window).require('electron');
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ImagesService {
+  images = new BehaviorSubject<string[]>([]);
+  directory = new BehaviorSubject<string[]>([]);
+
+  constructor() {
+    electron.ipcRenderer.on('getImagesResponse', (event, images) => {
+      this.images.next(images);
+    });
+    electron.ipcRenderer.on('getDirectoryResponse', (event, directory) => {
+      this.directory.next(directory);
+    });
+  }
+
+  navigateDirectory(path) {
+    electron.ipcRenderer.send('navigateDirectory', path);
+  }
+}
